a729d83b1edf673c1a4a061b1594018f491b5b2f
ale <alexx1231@gmail.com>
2019-11-06 22:35:38 -0800

    camera working for ios

---
+++ SocialApp/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+#pragma warning disable RECS0006 // Empty namespace declaration is redundant
+namespace SocialApp.ViewModels
+{
+
+}
+#pragma warning restore RECS0006 // Empty namespace declaration is redundant
