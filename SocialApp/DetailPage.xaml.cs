89b49606867de1d018b55f759b84d5e0707282b0
Alejandro Morales <43051685+alemor10@users.noreply.github.com>
2019-11-09 17:07:41 -0800

    navigation,views,modelviews,models
    navigation is set up, folders are now set up camera is now working on ios

---
+++ SocialApp/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SocialApp
+{
+    public partial class DetailPage : ContentPage
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- SocialApp/MainPage.xaml.cs
+++ SocialApp/MainPage.xaml.cs
@@ -14,65 +14,11 @@ namespace SocialApp
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
         public MainPage()
         {
             InitializeComponent();
-
-            takePhoto.Clicked += async (sender, args) =>
-            {
-                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
-                {
-                    DisplayAlert("No camera", ":(No camera available.", "OK");
-                    return;
-                }
-                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
-                {
-                    Directory = "Test",
-                    SaveToAlbum = true,
-                    CompressionQuality = 75,
-                    CustomPhotoSize = 50,
-                    PhotoSize = PhotoSize.MaxWidthHeight,
-                    MaxWidthHeight = 2000,
-                    DefaultCamera = CameraDevice.Front
-                });
-
-                if (file == null)
-                    return;
-                await DisplayAlert("File Location", file.Path, "OK");
-
-                image.Source = ImageSource.FromStream(() =>
-                {
-                    var stream = file.GetStream();
-                    file.Dispose();
-                    return stream;
-                });
-            };
-            pickPhoto.Clicked += async (sender, args) =>
-            {
-                if (!CrossMedia.Current.IsPickPhotoSupported)
-                {
-                    DisplayAlert("Photos Not Supported", ":( Permission not granted to photos.", "OK");
-                    return;
-                }
-                var file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
-                {
-                    PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
-
-                });
-
-
-                if (file == null)
-                    return;
-
-                image.Source = ImageSource.FromStream(() =>
-                {
-                    var stream = file.GetStream();
-                    file.Dispose();
-                    return stream;
-                });
-            };
         }
-    }
+    }     
 }
