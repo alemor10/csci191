e09a6749e98d6334c85a17c497488be6ee1bbac1
ale <alexx1231@gmail.com>
2019-11-17 13:57:17 -0800

    adding MVVM

---
+++ SocialApp/ModelViews/PicturesViewModel.cs
@@ -0,0 +1,57 @@
+﻿using SocialApp.Models; 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+
+namespace SocialApp.ModelViews
+{
+    public class PicturesViewModel : INotifyPropertyChanged
+    {
+        private PicturePost post = new PicturePost();
+
+        public PicturePost PicturePost
+        {
+            get { return post; }
+            set
+            {
+                post = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command SaveCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    if (PicturePost.PictureTitle == "Hello" && PicturePost.PictureCategory == "1")
+                    {
+                        App.Current.MainPage.DisplayAlert("Notification", "Successfully Login", "Okay");
+                        // Open next page
+                    }
+                    else
+                    {
+                        App.Current.MainPage.DisplayAlert("Notification", "Error Login", "Okay");
+                    }
+                });
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ SocialApp/Models/Pictures.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+
+namespace SocialApp.Models
+{
+    public class PicturePost
+    {
+        public string PictureTitle { get; set; }
+        public string PictureCategory { get; set; }
+        public string PictureLocation { get; set; }
+        public string PictureTime { get; set; }
+        public string PictureRating { get; set; }
+
+    }
+
+}
